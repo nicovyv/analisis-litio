a0c004fdd327cba0a95282bd467bf30cf3fb045f
nicolas zabala <nicozb.v@gmail.com>
2026-01-25 21:29:46 -0300

    ETL con python y SQL para datos de Litio

---
+++ etl_final.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import urllib
+
+SERVER_NAME = 'DESKTOP-SHHQA4R\\SQLEXPRESS'
+DATABASE_NAME = 'LitioDB'
+
+params = urllib.parse.quote_plus(
+    f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+    f'SERVER={SERVER_NAME};DATABASE={DATABASE_NAME};'
+    f'Trusted_Connection=yes;TrustServerCertificate=yes;'
+)
+
+engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
+print("Iniciando proceso de carga...")
+
+
+print("Preparando tabla de precios...")
+df_precios = pd.DataFrame({
+    'anio': [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
+    'precio_usd_ton': [6000, 9000, 12000, 14000, 10000, 10100, 14200, 71100, 41300, 14000]
+}) 
+
+print("Preparando tabla países")
+df_paises = pd.DataFrame({
+    'nombre_pais': ['Argentina', 'Chile', 'Bolivia', 'Australia', 'China', 'United States', 'Brazil', 'Canada', 'Zimbabwe', 'Portugal'],
+    'metodo_id':   [1,           1,       1,         2,           3,       1,               2,        2,        2,          2],
+    'reservas_ton':[3600000,     9300000, 23000000,  4800000,     3000000, 1100000,         390000,   1200000,  480000,     270000]
+})
+
+print("Leyendo CSV producción")
+df_prod = pd.read_csv('data/raw/lithium-production.csv')
+
+lista_paises_validos = df_paises['nombre_pais'].tolist()
+df_prod = df_prod[df_prod['Entity'].isin(lista_paises_validos)].copy()
+
+df_prod = df_prod[['Entity', 'Year', 'Lithium production - kt']]
+df_prod.columns = ['nombre_pais', 'anio', 'toneladas_producidas']
+
+df_prod = df_prod[df_prod['anio'].between(2015,2025)]
+
+try:
+    print("Insertando en SQL Server...")
+
+    df_precios.to_sql('dim_precios', con=engine, if_exists='append', index=False)
+    print("Precios cargados.")
+
+    df_paises.to_sql('dim_pais_detalles', con=engine, if_exists='append', index=False)
+    print("   ✅ Países cargados.")
+    
+    df_prod.to_sql('fact_produccion', con=engine, if_exists='append', index=False)
+    print("   ✅ Producción cargada.")
+
+    print('\n Proceso terminado. Base de datos cargada')
+
+except Exception as e:
+    print(f'\n ocurrió un error: {e}')
---
+++ sql_scripts/01_crear_tablas.sql
@@ -0,0 +1,45 @@
+USE LitioDB
+
+GO
+
+CREATE TABLE dim_metodo_extraccion (
+    metodo_id INT IDENTITY (1,1),
+    nombre_metodo VARCHAR (100) NOT NULL,
+    litros_agua_ton INT,
+    CONSTRAINT PK_dim_metodo_extraccion PRIMARY KEY (metodo_id)
+)
+
+GO
+
+CREATE TABLE dim_precios (
+    anio INT,
+    precio_usd_ton DECIMAL (10,2),
+    CONSTRAINT PK_dim_precios PRIMARY KEY (anio)
+)
+GO
+
+CREATE TABLE dim_pais_detalles (
+    nombre_pais VARCHAR (100) NOT NULL, -- LO MEJOR ES USAR EL NOMBRE DE PAIS (Entity) como ID para que reconozca el csv
+    metodo_id INT,
+    reservas_ton BIGINT,
+    CONSTRAINT PK_dim_pais_detalles PRIMARY KEY (nombre_pais),
+    CONSTRAINT FK_dim_pais_detalles_dim_metodo_extraccion FOREIGN KEY (metodo_id) REFERENCES dim_metodo_extraccion (metodo_id)
+)
+
+GO
+
+CREATE TABLE fact_produccion (
+    prod_id INT IDENTITY(1,1),
+    nombre_pais VARCHAR (100) NOT NULL,
+    anio INT,
+    toneladas_producidas DECIMAL(12,2),
+    CONSTRAINT PK_fact_produccion PRIMARY KEY (prod_id),
+    CONSTRAINT FK_fact_produccion_dim_pais_detalles FOREIGN KEY (nombre_pais) REFERENCES dim_pais_detalles(nombre_pais),
+    CONSTRAINT FK_fact_produccion_Anio FOREIGN KEY (anio) REFERENCES dim_precios(anio)
+);
+GO
+
+INSERT INTO dim_metodo_extraccion (nombre_metodo, litros_agua_ton) 
+VALUES ('Salmuera', 20000), ('Roca', 140000), ('Mixto China (70s/30r)', 104000);
+
+GO
